49acc0c38a85ace88f4d0eb28e54d14d6d374ee4
Fabrizia Percan <fabriziapercan@gmail.com>
2023-03-07 18:31:34 +0100

    first commit

---
+++ src/components/DrugKompa.jsx
@@ -0,0 +1,10 @@
+const DrugaKompa = () => {
+    return (
+        <div>
+            <h3>Druga komponenta</h3>
+            <p>valjda cu jednog dana shvatit i naucit ovo</p>
+        </div>
+    );
+};
+
+export default DrugaKompa;
